c8bb98ebb863c3a701a99caea405718097cc7d19
Alexis <alexis.giard@gmail.com>
2017-03-04 13:01:55 +0100

    progressing on the boids basic update logic before implementing the rules

---
+++ Assets/Scripts/Boids/Boid.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace aquab.boid{
+	
+	public class Boid : MonoBehaviour {
+
+		static float visionRadius = 1f;
+		static float personalSpace = 0.1f;
+
+		int localBoidsNumber;
+		Vector3[] localBoidsPos;
+
+		Vector3 velocity = Vector3.zero;
+
+
+		public void UpdatePosition () {
+			transform.position += velocity;
+		}
+			
+		public void InitialiseBoidMemory(int populationSize) {
+			localBoidsPos = new Vector3[populationSize];
+		}
+
+		public void ComputeVelocityFromSurroundings(Collider[] localBoids) {
+			CheckSuroundings (localBoids);
+			ComputeVelocity ();
+		}
+
+		void CheckSuroundings(Collider[] localBoids) {
+
+			localBoidsNumber = Physics.OverlapSphereNonAlloc (transform.position, visionRadius, localBoids);
+
+			int j = 0;
+			for (int i = 0; i < localBoidsNumber; i++) {
+				
+				if (localBoids [i] != null && localBoids [i].gameObject != this.gameObject) {
+					localBoidsPos[j] = localBoids [i].transform.position;
+					j++;
+
+				} else if (localBoids [i] == null) {
+					break;
+				}
+			}
+			 
+			localBoidsNumber--; //-1 as the number includes this boid as well
+		}
+
+		void ComputeVelocity() {
+			velocity += SeparationRule() + AlignmentRule() + CohesionRule();
+		}
+
+		Vector3 SeparationRule() {
+			return Vector3.zero;
+		}
+		
+		Vector3 AlignmentRule () {
+			return Vector3.zero;
+		}
+		
+		Vector3 CohesionRule () {
+			return Vector3.zero;
+		}
+
+	}
+}
---
+++ Assets/Scripts/Boids/School.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace aquab.boid {
+	
+	public class School : MonoBehaviour {
+
+		[SerializeField] GameObject fish;
+		[SerializeField] int popuplation = 100;
+
+		[SerializeField] int aquariumSize = 5;
+		Vector3 aquariumDimensions;
+
+		Boid[] fishes;
+		Collider[] temporaryLocalBoidsReference;
+
+
+		// Use this for initialization
+		void Start () {
+			CreatePopulation();
+			aquariumDimensions = new Vector3(aquariumSize*2f, aquariumSize*2f, aquariumSize*2f);
+
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			UpdateBoidsVelocity ();
+			UpdateBoidsPosition ();
+		}
+
+		void CreatePopulation () {
+			fishes = new Boid[popuplation];
+			temporaryLocalBoidsReference = new Collider[popuplation];
+
+			for(int i = 0; i < popuplation; i++) {
+				GameObject newFish = Instantiate(fish);
+				newFish.transform.parent = transform;
+				newFish.transform.position = Random.insideUnitSphere * aquariumSize;
+				newFish.transform.rotation = Quaternion.Euler(Random.Range(-20f, 20f), Random.Range(0f, 360f), 0f );
+				fishes[i] = newFish.GetComponent<Boid>();
+			}
+		}
+
+		void UpdateBoidsVelocity() {
+			for(int i = 0; i < popuplation; i++) {
+				fishes[i].ComputeVelocityFromSurroundings(temporaryLocalBoidsReference);
+			}
+		}
+
+		void UpdateBoidsPosition () {
+			for(int i = 0; i < popuplation; i++) {
+				fishes [i].UpdatePosition ();
+			}
+		}
+
+	}
+
+}
